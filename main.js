b8893bb786125a9527b7167c774432962158b4d9
arsche <thegdz9@gmail.com>
2022-11-25 12:08:23 +0200

    Update (Nov)
    - favicon
    - "open new tab" option
    - styles
    - other minor changes

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let new_tab_checkbox = document.getElementById('new-tab-checkbox');
+  if (localStorage.getItem('open_new_tab') === 'true') {
+    new_tab_checkbox.checked = true;
+  } else if (localStorage.getItem('open_new_tab') === 'false') {
+    new_tab_checkbox.checked = false
+  } else {
+    // default
+    new_tab_checkbox.checked = true;
+  }
+});
+
+function on_new_tab_checkbox(e) {
+  open_new_tab = e.checked;
+  table_set_target(open_new_tab);
+  localStorage.setItem('open_new_tab', open_new_tab);
+}
+
+function table_set_target(open_new_tab) {
+  let table = document.querySelectorAll("table tr td a");
+  for (let x of table) {
+    x.target = `${open_new_tab ? '_blank' : '_self'}`;
+  }
+}
